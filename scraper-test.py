7d17a0a88e395dfb9e004b4092cf955e74314cf3
caroline710 <caroline.ruggiero710@gmail.com>
2023-11-27 23:46:33 +0100

    scraped all of 2019

--- scraper-test.py
+++ scraper-test.py
@@ -7,26 +7,26 @@ from twscrape.logger import set_log_level
 
 async def main():
     api = API()
-    year = '2019'
+    year = '2020'
     months = range (1, 13)
 
     for month in months:
-        days = random.sample(range(1,29), 5) # select 5 random days
+        days = random.sample(range(1,30), 10) # select 10 random days
 
         for day in days:
             start_date = datetime.date(int(year), month, day)
             end_date = start_date + datetime.timedelta(days=1) # next day
-            q = f"Greta Thunberg since:{start_date} until:{end_date}"
+            q = f"Greta Thunberg OR climate change since:{start_date} until:{end_date}"
             # print(f"Querying: {q}")
 
-            with open('tweets-random-2019.csv', mode='a', newline='', encoding='utf-8') as file:
+            with open('tweets-2020-again.csv', mode='a', newline='', encoding='utf-8') as file:
 
                 writer = csv.writer(file)
                 if file.tell() == 0:
                     writer.writerow(['Tweet ID', 'Username', 'Content', 'Created At', 'User Location'])
 
                 tweet_count = 0
-                async for tweet in api.search(q, limit=500): # sets limit to 500 tweets per day
+                async for tweet in api.search(q, limit=1000): # sets limit to 1000 tweets per day
                     user_profile = await api.user_by_id(tweet.user.id)
 
                     user_location = user_profile.location
@@ -38,7 +38,7 @@ async def main():
                         writer.writerow([tweet.id, tweet.user.username, tweet.rawContent, tweet.date, user_location])
 
                         tweet_count+=1
-                        if tweet_count >= 500:
+                        if tweet_count >= 1000:
                             break
                 
                 print(f"Number of tweets collected for {start_date}: {tweet_count}")
